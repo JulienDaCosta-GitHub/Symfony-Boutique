b8857a7e06187ecf68c60bcf2c8677b7d685c96e
JulienDaCosta-GitHub <55091161+JulienDaCosta-GitHub@users.noreply.github.com>
2020-03-25 18:04:29 +0100

    make it better

--- src/Controller/BrandNewController.php
+++ src/Controller/BrandNewController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class BrandNewController extends AbstractController
 {
     /**
-     * @Route("/brand/new", name="brand_new")
+     * @Route("/", name="brand_new")
      */
     public function index()
     {
